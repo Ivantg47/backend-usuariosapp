f14fc9b3360e65f5eade5ed0342f55f5c655245c
Ivan <ivan_38@comunidad.unam.mx>
2025-03-26 12:35:34 -0600

    init

---
+++ src/main/java/com/tagi/backend/usuariosapp/backend_usuariosapp/BackendUsuariosappApplication.java
@@ -0,0 +1,13 @@
+package com.tagi.backend.usuariosapp.backend_usuariosapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendUsuariosappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendUsuariosappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tagi/backend/usuariosapp/backend_usuariosapp/BackendUsuariosappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tagi.backend.usuariosapp.backend_usuariosapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendUsuariosappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
