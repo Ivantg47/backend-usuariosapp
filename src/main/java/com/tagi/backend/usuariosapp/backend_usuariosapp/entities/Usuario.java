62e2dde196818f39c34aa34b9ac84f3846ff68cb
XMF3816 <ivanantonio.tronco.contractor@bbva.com>
2025-03-28 14:46:02 -0600

    modifico tabla agrego env

--- src/main/java/com/tagi/backend/usuariosapp/backend_usuariosapp/entities/Usuario.java
+++ src/main/java/com/tagi/backend/usuariosapp/backend_usuariosapp/entities/Usuario.java
@@ -12,7 +12,7 @@ import jakarta.persistence.Table;
 public class Usuario {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(unique = true)
